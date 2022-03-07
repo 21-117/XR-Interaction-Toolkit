51fd56b876334584949c4ed68af75c856e6ca441
JLee <99634045+21-117@users.noreply.github.com>
2022-03-07 23:52:19 +0900

    Teleport

--- XR_Project/Assets/Scripts/Controller_Moving.cs
+++ XR_Project/Assets/Scripts/Controller_Moving.cs
@@ -5,17 +5,60 @@ using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 public class Controller_Moving : MonoBehaviour
 {
+    #region Private Variables
+    private XRController xrCont;
+    private Transform xrRig;
+    private int value = 0;
+    private float speed = 0.5f;
+    #endregion
 
+    #region Public Variables
+    public Transform reticle_obj;
+    #endregion
 
-    // Start is called before the first frame update
     void Start()
     {
+        // 태그로 설정 된 오른쪽 컨트롤러 할당
+        xrCont = GameObject.FindWithTag("Right").transform.GetComponent<XRController>();
+        xrRig = GameObject.FindWithTag("XRRig").transform;
+    }
+
+    void Update()
+    {
+        //스위치 문으로 이동 컨트롤
+        switch (value)
+        {
+            case 0:
+                SetClear();
+                break;
+            case 1:
+                XRRig_Teleport();
+                break;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetClear()
     {
-        
+
     }
+
+    void XRRig_Teleport()
+    {
+        // 오른손으로 가리키는 위치 클릭 받기
+        if (xrCont.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool trigger))
+        {
+            if (trigger)
+            {
+                // XR 리그 이동
+                xrRig.transform.position = reticle_obj.transform.position;
+                xrRig.transform.rotation = reticle_obj.transform.rotation;
+
+                // 트리거 버튼 후 초기화
+                // 추후 픽스 예정
+                value = 0;
+            }
+        }
+    }
+
 }
