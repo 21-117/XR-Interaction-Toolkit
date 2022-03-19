81b76d8b5ac3f71b097121da6dd60a1817f6ac9f
JLee <99634045+21-117@users.noreply.github.com>
2022-03-20 01:06:28 +0900

    ID MATCH

--- XR_Project/Assets/SampleSceneAssets/Scripts/ObjectID.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjectID : MonoBehaviour
-{
-    public EnumDefinition.TYPE aaa;
-    public int num;
-}
---
+++ XR_Project/Assets/Scripts/SocketWithObj.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class SocketWithObj : XRSocketInteractor
+{
+    public ObjectID objid;
+    public EnumDefinition.TYPE type;
+    public int numbers;
+    void Start()
+    {
+        GetID();
+    }
+
+    public override bool CanHover(IXRHoverInteractable interactable)
+    {
+        return base.CanHover(interactable);
+    }
+
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && IsMatchSelect(interactable);
+    }
+
+    void GetID()
+    {
+        this.transform.TryGetComponent<ObjectID>(out ObjectID id);
+    }
+
+    public bool IsMatchSelect(IXRSelectInteractable interactable)
+    {
+        if (interactable.transform.TryGetComponent<ObjectID>(out ObjectID id))
+        {
+            bool isMatch = id.aaa == type;
+            
+            if (isMatch) {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+ 
+}
