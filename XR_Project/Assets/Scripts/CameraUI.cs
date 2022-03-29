50bb54d5966166a21adc51645cf04f65f23d9c3f
JLee <99634045+21-117@users.noreply.github.com>
2022-03-29 23:39:41 +0900

    .

---
+++ XR_Project/Assets/Scripts/CameraUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraUI : MonoBehaviour
+{
+    public GameObject progress_sub;
+    public GameObject caution;
+    
+    public Text progress;
+    public Text etc;
+
+    public Image bg;
+
+}
