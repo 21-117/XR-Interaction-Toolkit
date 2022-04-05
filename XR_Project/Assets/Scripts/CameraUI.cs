7f305fe3789ebc62cbd4b4f6d69b2f43edd83280
JLee <99634045+21-117@users.noreply.github.com>
2022-04-05 23:10:19 +0900

    add Tracking Camera

--- XR_Project/Assets/Scripts/CameraUI.cs
+++ XR_Project/Assets/Scripts/CameraUI.cs
@@ -5,12 +5,32 @@ using UnityEngine.UI;
 
 public class CameraUI : MonoBehaviour
 {
-    public GameObject progress_sub;
-    public GameObject caution;
-    
-    public Text progress;
-    public Text etc;
+    public GameObject mainCamera;
+    public GameObject cameraUI;
 
-    public Image bg;
+    public float offsetRadius = 0.3f;
+    public float distanceToHead = 4;
 
+    [Range(0, 10)]
+    public float smoothPosFactor = 5f;
+
+    [Range(0, 10)]
+    public float smoothRotFactor = 5f;
+
+    [Range(0, 1)]
+    public float leftPosition = 0.2f;
+
+    void Update()
+    {
+
+        cameraUI.transform.rotation = Quaternion.Lerp(transform.rotation, mainCamera.transform.rotation, smoothRotFactor);
+
+        var cameraCenter = mainCamera.transform.position + mainCamera.transform.forward * distanceToHead - mainCamera.transform.right * leftPosition;
+        var currentPos = cameraUI.transform.position;
+        var direction = currentPos - cameraCenter; 
+        var targetPosition = cameraCenter + direction.normalized * offsetRadius;
+
+        cameraUI.transform.position = Vector3.Lerp(currentPos, targetPosition, Time.deltaTime * smoothPosFactor);
+
+    }
 }
