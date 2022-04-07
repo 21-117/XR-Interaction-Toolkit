adf8d241b161747e159c53bc649ef4afcc5866ce
JLee <99634045+21-117@users.noreply.github.com>
2022-04-07 23:59:38 +0900

    add sdm

--- XR_Project/Assets/Scripts/Scenario_Manager.cs
+++ XR_Project/Assets/Scripts/Scenario_Manager.cs
@@ -2,19 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scenario_Manager : MonoBehaviour
+public class Data_Manager : MonoBehaviour
 {
-    public Scenario_Data manager_component;
-    void Start()
-
-    {
-        manager_component.manager_Delegate += SequenceClear;
-
-    }
-
-    public void SequenceClear()
-
-    {
-        manager_component.index++;
-    }
+    public static Data_Manager instance;
 }
